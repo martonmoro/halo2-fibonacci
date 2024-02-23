99c51ee2f4baf23b6cc8dd8ed3b9dcf76b54e955
Marton Moro <marton.moro@dakai.io>
2024-02-23 10:59:42 +0100

    add example2 (single column fibo)

--- src/example2.rs
+++ src/example2.rs
@@ -1,5 +1,6 @@
 /*
-Basic three column fibonacci circuit
+Single column fibonacci circuit
+This leads to faster proof generation
 */
 
 use std::marker::PhantomData;
@@ -26,7 +27,8 @@ struct ACell<F: FieldExt>(AssignedCell<F, F>);
 // Selector columns hold control gates (binary constants) - circuit configuration
 #[derive(Debug, Clone)]
 struct FiboConfig {
-    pub advice: [Column<Advice>; 3],
+    // In this example we are using only one column in order to try to avoid permutation checks
+    pub advice: Column<Advice>,
     pub selector: Selector,
     pub instance: Column<Instance>
 }
@@ -50,120 +52,105 @@ impl<F: FieldExt> FiboChip<F> {
     //  - Complex Selector - used in lookup arguments, selector has to always be binary thus cannot apply selector combining optimizations
     fn configure(
         meta: &mut ConstraintSystem<F>, 
-        advice: [Column<Advice>; 3], 
+        advice: Column<Advice>,
         instance: Column<Instance>
     ) -> FiboConfig {
         // We pass them in because they can be shared accross different configs
-        let col_a = advice[0];
-        let col_b = advice[1];
-        let col_c = advice[2];
+        // In this example we only have one advice column
         let selector = meta.selector();
 
         // Enable the ability to enforce equality over cells in this column
         // enabling equality has some cost so we only want to enable it when neccessary
-        meta.enable_equality(col_a);
-        meta.enable_equality(col_b);
-        meta.enable_equality(col_c);
+        // We still need to enable equality on the advice column because 
+        // we want to check that the first and last elements are the same as in the instance column
+        meta.enable_equality(advice);
         meta.enable_equality(instance);
 
         // Defining a create_gate here applies it over every single column in the circuit.
         // We will use the selector column to decide when to turn this gate on and off, since we probably don't want it on every row
         meta.create_gate("add", |meta| {
             //
-            // col_a | col_b | col_c | selector
-            //   a      b        c       s
-            //
+            // col_a | selector
+            //   a   |    s
+            //   b   |    
+            //   c   |    
             let s = meta.query_selector(selector);
-            let a = meta.query_advice(col_a, Rotation::cur()); // curr means we are using the field from the same row as the selector 
-            let b = meta.query_advice(col_b, Rotation::cur());
-            let c = meta.query_advice(col_c, Rotation::cur());
+            // In this example all of the checks are from the same column but from three different rows frollowing each other
+            let a = meta.query_advice(advice, Rotation::cur()); // curr means we are using the field from the same row as the selector 
+            let b = meta.query_advice(advice, Rotation::next());
+            let c = meta.query_advice(advice, Rotation(2));
             // return the constraint
             // there can be multiple constraints
             vec![s * (a + b - c)]
         });
 
         FiboConfig {
-            advice: [col_a, col_b, col_c],
+            advice,
             selector,
             instance,
         }
     }
 
-    // These assign functions are to be called by the synthesizer, and will be used to assign values to the columns (the witness)
-    // The layouter will collect all the region definitions and compress it vertically (i.e. squeeze up/down)
-    // but not horizontally (i.e. will not squeeze left/right, at least right now)
-    fn assign_first_row(&self, mut layouter: impl Layouter<F>, a: Option<F>, b: Option<F>) -> Result<(ACell<F>, ACell<F>, ACell<F>), Error>{
+    // In this example we cannot assign row-by-row so we are assigning the entire table in one function
+    fn assign(
+        &self, 
+        mut layouter: impl Layouter<F>, 
+        nrows: usize,
+    ) -> Result<AssignedCell<F, F>, Error>{
         layouter.assign_region(
-            || "first_row", 
+            || "entire fibonacci table", 
             |mut region| {
                 self.config.selector.enable(&mut region, 0)?;
 
-                let a_cell = region.assign_advice(
-                    || "a", 
-                    self.config.advice[0], 
-                    0, 
-                    || a.ok_or(Error::Synthesis),
-                ).map(ACell)?;
+                let mut a_cell = region.assign_advice_from_instance(
+                    || "1",
+                    self.config.instance,
+                    0,
+                    self.config.advice,
+                    0
+                )?;
 
-                let b_cell = region.assign_advice(
-                    || "b", 
-                    self.config.advice[1], 
-                    0, 
-                    || b.ok_or(Error::Synthesis),
-                ).map(ACell)?;
+                let mut b_cell = region.assign_advice_from_instance(
+                    || "1",
+                    self.config.instance,
+                    1,
+                    self.config.advice,
+                    1
+                )?;
 
-                let c_val = a.and_then(|a| b.map(|b| a + b));
+                for row in 2..nrows {
+                    if row < nrows - 2 {
+                        self.config.selector.enable(&mut region, row)?;
+                    }
+                    let c_val = a_cell.value().and_then(
+                        |a| {
+                            b_cell.value().map(|b| *a + *b)
+                        }
+                    );
 
-                let c_cell = region.assign_advice(
-                    || "c", 
-                    self.config.advice[2], 
-                    0, 
-                    || c_val.ok_or(Error::Synthesis),
-                ).map(ACell)?;
+                    let c_cell = region.assign_advice(
+                        || "advice", 
+                        self.config.advice, 
+                        row, 
+                        || c_val.ok_or(Error::Synthesis)
+                    )?;
 
-               Ok((a_cell, b_cell, c_cell))
+                    a_cell = b_cell;
+                    b_cell = c_cell;
+                }
+
+               Ok(b_cell)
             })
     }
 
-    // This will be repeatedly called. Note that each time it makes a new region, comprised of a, b, c, s that happen to all be in the same row
-    fn assign_row(&self, mut layouter: impl Layouter<F>, prev_b: &ACell<F>, prev_c: &ACell<F>) -> Result<ACell<F>, Error> {
-        layouter.assign_region(
-            || "next row",
-            |mut region| {
-                // first we enable the selector
-                self.config.selector.enable(&mut region, 0)?;
-
-                // copy from previous assigned cells
-                // copy to the current region
-                // permutation check
-                prev_b.0.copy_advice(|| "a", &mut region, self.config.advice[0], 0)?;
-                prev_c.0.copy_advice(|| "b", &mut region, self.config.advice[1], 0)?;
-
-                let c_val = prev_b.0.value().and_then(
-                    |b|  {
-                        prev_c.0.value().map(|c| *b + *c)
-                    }
-                );
-                
-                let c_cell = region.assign_advice(
-                    || "c", 
-                    self.config.advice[2], 
-                    0, 
-                    || c_val.ok_or(Error::Synthesis)
-                ).map(ACell)?;
-
-                Ok(c_cell)
-            }
-        )
-    }
 
     pub fn expose_public(
         &self, 
         mut layouter: impl Layouter<F>,
-        cell: &ACell<F>,
+        cell: AssignedCell<F, F>,
         row: usize,         // Absolute index inside the instance column
     ) -> Result<(), Error> {
-        layouter.constrain_instance(cell.0.cell(), self.config.instance, row)
+        layouter.constrain_instance(cell.cell(), self.config.instance, row)
     }
 }
 
@@ -186,11 +173,9 @@ impl<F: FieldExt> Circuit<F> for MyCircuit<F> {
 
     // Has the arrangement of columns. Called only during keygen, and will just call chip config most of the time
     fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
-        let col_a = meta.advice_column();
-        let col_b = meta.advice_column();
-        let col_c = meta.advice_column();
+        let advice = meta.advice_column();
         let instance = meta.instance_column();
-        FiboChip::configure(meta, [col_a, col_b, col_c], instance)
+        FiboChip::configure(meta, advice, instance)
     }
 
     // Take the output of configure and floorplanner type to make the actual circuit
@@ -199,25 +184,12 @@ impl<F: FieldExt> Circuit<F> for MyCircuit<F> {
     fn synthesize(&self, config: Self::Config, mut layouter: impl Layouter<F>) -> Result<(), Error> {
         let chip = FiboChip::construct(config);
 
-        let (prev_a, mut prev_b, mut prev_c) = chip.assign_first_row(
-            layouter.namespace(|| "first row"),
-            self.a, self.b,
+        let out_cell = chip.assign(
+            layouter.namespace(|| "entire table"),
+            10
         )?;
 
-        chip.expose_public(layouter.namespace(|| "private a"), &prev_a, 0)?;
-        chip.expose_public(layouter.namespace(|| "private b"), &prev_b, 1)?;
-
-        for _i in 3..10 {
-            let c_cell = chip.assign_row(
-                layouter.namespace(|| "next row"),
-                &prev_b,
-                &prev_c,
-            )?;
-            prev_b = prev_c;
-            prev_c = c_cell; 
-        }
-
-        chip.expose_public(layouter.namespace(|| "output"), &prev_c, 2)?;
+        chip.expose_public(layouter.namespace(|| "out"), out_cell, 2)?;
 
         Ok(())
     }
